6e4b9d9c9230cc45faed1c541af6adff870e6eb1
Ben <bkunkle@calpoly.edu>
2023-01-04 12:52:09 -0600

    feat(benchmarks): replace printing with logging

--- benchmarks/benchers/scikit_image_bench.py
+++ benchmarks/benchers/scikit_image_bench.py
@@ -1,6 +1,8 @@
 import numpy as np
 from skimage import io, transform, exposure, filters
 from skimage.color import rgb2gray, rgba2rgb
+import logging
+from utils import ansi
 
 import pathlib
 import sys
@@ -11,64 +13,59 @@ sys.path.append(str(cur_lib_path))
 
 
 def greyscale_and_transpose_performance(img, img_name):
-    print(
-        '\033[95m' + f"\nGreyscaling and transposing {img_name} using SciKit-Image\n" + '\033[0m')
+    logging.info( ansi(f"\nGreyscaling and transposing {img_name} using SciKit-Image\n"))
 
     start = time.perf_counter()
     grey_img = rgb2gray(rgb2gray(rgba2rgb(img)))
     transform.rotate(grey_img, 90)
     stop = time.perf_counter()
 
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def greyscale_performance(img, img_name):
-    print('\033[95m' +
+    logging.info('\033[95m' +
           f"\nGreyscaling {img_name} using SciKit-Image\n" + '\033[0m')
     start = time.perf_counter()
     img = rgb2gray(rgba2rgb(img))
     stop = time.perf_counter()
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def transpose_performance(img, img_name):
 
-    print('\033[95m' +
-          f"\nTransposing {img_name} using SciKit-Image\n" + '\033[0m')
+    logging.info(ansi(f"\nTransposing {img_name} using SciKit-Image\n"))
     start = time.perf_counter()
     img = np.transpose(img)
     stop = time.perf_counter()
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def rot_performance(img, img_name):
-    print('\033[95m' +
-          f"\nRotating {img_name} using SciKit-Image\n" + '\033[0m')
+    logging.info(ansi(f"\nRotating {img_name} using SciKit-Image\n"))
     start = time.perf_counter()
     img = transform.rotate(img, 45)
     stop = time.perf_counter()
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def gamma_performance(img, img_name):
-    print('\033[95m' +
-          f"\nAdjusting the gamma of {img_name} using SciKit-Image\n" + '\033[0m')
+    logging.info(ansi(f"\nAdjusting the gamma of {img_name} using SciKit-Image\n"))
     img = rgb2gray(img)
 
     start = time.perf_counter()
     img = exposure.adjust_gamma(img, 2, 1)
     stop = time.perf_counter()
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def gauss_performance(img, img_name):
-    print('\033[95m' +
-          f"\nDoing Gaussian blur on {img_name} using SciKit-Image\n" + '\033[0m')
+    logging.info(ansi(f"\nDoing Gaussian blur on {img_name} using SciKit-Image\n"))
     img = rgb2gray(img)
     start = time.perf_counter()
     img = filters.gaussian(img, sigma=2, cval=0, truncate=8, mode="constant")
     stop = time.perf_counter()
-    print("\nTime to convert image: {}\n".format(stop - start))
+    logging.info("\nTime to convert image: {}\n".format(stop - start))
 
 
 def main():
