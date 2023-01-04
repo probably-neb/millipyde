3919753fd24d57e67bf3e83ba7711f58bbf56a03
Ben <bkunkle@calpoly.edu>
2023-01-04 16:04:40 -0600

    feat(benchmarks): autoload '*_bench.py' files from benchers pkg
    fixup!

--- benchmarks/benchers/__init__.py
+++ benchmarks/benchers/__init__.py
@@ -1,1 +1,20 @@
-from . import *
+# from .millipyde_bench import *
+# from .scikit_image_bench import *
+# from .pillow_bench import *
+import os
+import glob
+import importlib
+
+
+def load_benchers():
+    # TODO: replace endswith("_bench.py")
+    bencher_fs_paths = list(
+        filter(lambda p: p.endswith("_bench.py"),
+               os.listdir(os.path.dirname(__file__)))
+    )
+    bencher_mod_paths = [__package__ + '.' + p.replace(".py", "")
+                         for p in bencher_fs_paths]
+    print(bencher_mod_paths)
+    mods = [importlib.import_module(mod_path)
+            for mod_path in bencher_mod_paths]
+    return mods
