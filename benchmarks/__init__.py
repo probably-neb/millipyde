9db574dbe356623da4785b24b2218f4dd7f5d4eb
Ben <bkunkle@calpoly.edu>
2023-01-04 12:51:21 -0600

    fix(benchmarks): remove top level __init__.py

--- benchmarks/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import pathlib
-
-cur_lib_path = pathlib.Path().absolute()
-sys.path.append(str(cur_lib_path))
-import millipyde as mp
-
-INPUTS_PATH = str(cur_lib_path.joinpath("benchmarks/inputs"))
-OUTPUTS_PATH = str(cur_lib_path.joinpath("benchmarks/outputs"))
