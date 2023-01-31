f5d39efdda19232e99f04c27340e2e6003a66102
Ben <bkunkle@calpoly.edu>
2023-01-31 12:16:42 -0600

    fix: get rotate working

--- benchmarks/benchers/opencv_cuda.py
+++ benchmarks/benchers/opencv_cuda.py
@@ -4,33 +4,36 @@ from .bencher_interface import Benchmarker, benchmark
 
 assert cv2.cuda.getCudaEnabledDeviceCount() > 0, "OpenCV Wheels Not Found"
 
+
 class OpenCVCudaBenchmarker(Benchmarker):
     name = "OpenCV-Cuda"
 
     def load_image_from_path(path: str):
         image = cv2.imread(path, cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGBA)
-        # to get it in the same format everything else uses
+
+        # send image to gpu
         frame = cv2.cuda_GpuMat()
         frame.upload(image)
 
         return frame
 
     @classmethod
-    def output_image_to_np_array(cls, output_image, benchmark_name: str):
-        image = output_image.download()
+    def output_image_to_np_array(cls, frame, benchmark_name: str):
+        # get image back from gpu
+        image = frame.download()
         return super().output_image_to_np_array(image, benchmark_name)
 
     @benchmark
-    def rgb_to_grayscale(image):
-        return cv2.cuda.cvtColor(image, cv2.COLOR_RGBA2GRAY)
+    def rgb_to_grayscale(frame):
+        return cv2.cuda.cvtColor(frame, cv2.COLOR_RGBA2GRAY)
 
     @benchmark
-    def transpose(image):
-        return cv2.cuda.transpose(image)
+    def transpose(frame):
+        return cv2.cuda.transpose(frame)
 
     @benchmark
-    def gauss_sigma_2(image):
+    def gauss_sigma_2(frame):
 
         # get datatype of cuda_GpuMat:
         # frame.type()
@@ -41,13 +44,13 @@ class OpenCVCudaBenchmarker(Benchmarker):
         filter = cv2.cuda.createGaussianFilter(
             cv2.CV_8UC4, cv2.CV_8UC4, ksize=(0, 0), sigma1=2, sigma2=2
         )
-        return filter.apply(image)
+        return filter.apply(frame)
         # return cv2.cuda.GaussianBlur(
         #     image, ksize=(0, 0), sigmaX=2, sigmaY=2, borderType=cv2.cuda.BORDER_CONSTANT
         # )
 
-    # @benchmark
-    def _rotate_90_deg(image):
+    @benchmark
+    def rotate_90_deg(frame):
         """taken from https://github.com/PyImageSearch/imutils/blob/master/imutils/convenience.py
          using this over the builtin opencv.rotate function to maintain consistent behavior with
          other tools:
@@ -61,12 +64,12 @@ class OpenCVCudaBenchmarker(Benchmarker):
                     | v |                | > |
                     | v |                |   |
         """
-        (h, w) = image.shape[:2]
+        # frame.size == np.array.shape[:2]
+        (h, w) = frame.size()
 
         center = (w // 2, h // 2)
 
-        # perform the rotation
-        M = cv2.cuda.getRotationMatrix2D(center, 90, 1.0)
-        rotated = cv2.cuda.warpAffine(image, M, (w, h))
+        M = cv2.getRotationMatrix2D(center, 90, 1.0)
+        rotated = cv2.cuda.warpAffine(frame, M, (w, h))
 
         return rotated
