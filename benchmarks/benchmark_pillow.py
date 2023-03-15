6e7dd817c2616ffa8219cbfa6595a470798144f2
Ben <bkunkle@calpoly.edu>
2023-03-15 16:33:46 -0500

    unavoidable diff for pillow.gauss

--- benchmarks/benchmark_pillow.py
+++ benchmarks/benchmark_pillow.py
@@ -6,6 +6,12 @@ def pil_image_from_ndarray(ndarray):
     return Image.fromarray(ndarray)
 
 
+def to_ndarray(image):
+    import numpy
+
+    return numpy.array(image)
+
+
 def rotate_90_deg(image):
     return image.rotate(90)
 
@@ -24,11 +30,19 @@ def gauss_sigma_2(image) -> float:
     return image.filter(ImageFilter.GaussianBlur(radius=2))
 
 
-def to_ndarray(image):
-    import numpy
+def compare_gauss_sigma_2(a, b):
+    raise utils.UnavoidableDifference(
+        "Pillow uses box filters to approximate a gaussian blur: https://pillow.readthedocs.io/en/stable/reference/ImageFilter.html#PIL.ImageFilter.GaussianBlur"
+    )
 
-    return numpy.array(image)
 
+utils.create_output_verifier(
+    gauss_sigma_2,
+    locals(),
+    image_from_ndarray=pil_image_from_ndarray,
+    image_to_ndarray=to_ndarray,
+    verify_output=compare_gauss_sigma_2,
+)
 
 locals()[utils.CONVERTER_FUNC_NAME] = to_ndarray
 
