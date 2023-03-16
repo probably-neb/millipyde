5f2633828d8933835b804202c3013320941b8cc0
Ben Kunkle <ben.kunkle@gmail.com>
2023-03-15 17:53:58 -0700

    fix(skimage): correct channel axis for gauss

--- benchmarks/benchmark_scikit_image.py
+++ benchmarks/benchmark_scikit_image.py
@@ -5,7 +5,8 @@ from skimage.color import rgb2gray, rgba2rgb
 from skimage.util import img_as_ubyte
 import numpy as np
 
-TRUNCATE=8
+TRUNCATE = 8
+
 
 # NOTE: this worked with just rgb2gray(image) in scikit-image=0.18.2 however
 # it raised a warning that rgb2gray(rgba2rgb(image)) was the proper way
@@ -17,6 +18,7 @@ def rgb_to_grayscale(image) -> float:
     # return rgb2gray(image)
     return rgb2gray(rgba2rgb(image))
 
+
 def transpose(image):
     # a transpose is equivalent to 90 degree rotation ccw
     # followed by a flip along the horizantal axis
@@ -24,35 +26,54 @@ def transpose(image):
     # resize is for consistency with other tools implementations
     return np.flipud(transform.rotate(image, 90, resize=True))
 
+
 def rotate_90_deg(image):
     return transform.rotate(image, 90)
 
-def compare_rotate_90(a,b):
-    raise utils.UnavoidableDifference(f"{utils.percent_mismatched(a,b):.4}% mismatch due to different handling of top and bottom edge (see README)")
+
+def compare_rotate_90(a, b):
+    raise utils.UnavoidableDifference(
+        f"{utils.percent_mismatched(a,b):.4}% mismatch due to different handling of top and bottom edge (see README)"
+    )
+
 
 utils.create_output_verifier(rotate_90_deg, locals(), verify_output=compare_rotate_90)
+
 
 def adjust_gamma_2_gain_1(image):
     return exposure.adjust_gamma(image, 2, 1)
 
+
 def gauss_sigma_2(image):
     return filters.gaussian(
-        image, sigma=2, cval=0, truncate=TRUNCATE, mode="constant", channel_axis=3
+        image, sigma=2, cval=0, truncate=TRUNCATE, mode="constant", channel_axis=2
     )
 
-def compare_gauss_sigma_2(actual,millipyde):
-    assert not np.all(actual[:,:,-1] == 1.0), "Assumed skimage gaussian blur did not set the alpha channel of each pixel to 1.0 but it did"
-    assert np.all(millipyde[:,:,-1] == 1.0), "Assumed millipyde gaussian blur set the alpha channel of each pixel to 1.0 bit it didn't"
+
+def compare_gauss_sigma_2(actual, millipyde):
+    assert not np.all(
+        actual[:, :, -1] == 1.0
+    ), "Assumed skimage gaussian blur did not set the alpha channel of each pixel to 1.0 but it did"
+    assert np.all(
+        millipyde[:, :, -1] == 1.0
+    ), "Assumed millipyde gaussian blur set the alpha channel of each pixel to 1.0 bit it didn't"
     argb = actual[:, :, :3]
     brgb = millipyde[:, :, :3]
-    raise utils.UnavoidableDifference(f"millipyde sets the alpha channel of every pixel to 1.0, skimage treats it as another channel. Diff between rgb channels is not consistent: mean: {np.mean(argb - brgb):.3} std dev: {np.std(argb-brgb):.3}")
+    raise utils.UnavoidableDifference(
+        f"millipyde sets the alpha channel of every pixel to 1.0, skimage treats it as another channel. Diff between rgb channels is not consistent: mean: {np.mean(argb - brgb):.3} std dev: {np.std(argb-brgb):.3}"
+    )
 
-utils.create_output_verifier(gauss_sigma_2, locals(), verify_output=compare_gauss_sigma_2)
+
+utils.create_output_verifier(
+    gauss_sigma_2, locals(), #verify_output=compare_gauss_sigma_2
+)
+
 
 def grayscale_gauss_sigma_2(image):
     return filters.gaussian(
         rgb_to_grayscale(image), sigma=2, cval=0, truncate=8, mode="constant"
     )
+
 
 # locals()[utils.CONVERTER_FUNC_NAME] = img_as_ubyte
 
