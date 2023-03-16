fdbf8ab4543591a26284f26c1166e48b464da648
Ben Kunkle <ben.kunkle@gmail.com>
2023-03-16 10:03:32 -0700

    make compare.py into cli script

--- benchmarks/compare.py
+++ benchmarks/compare.py
@@ -1,25 +1,56 @@
-import benchmark_cupy as bcp
-import benchmark_millipyde as bmp
-import benchmark_scikit_image as bsi
-import benchmark_pillow as bpl
 import utils
 import numpy as np
+import argparse
 
-a = utils.load_image_from_path("inputs/charlie12.png")
+DEFAULT_FUNC="gauss_sigma_2"
+DEFAULT_COMPARISON_IMAGE = "inputs/charlie12.png"
+DEFAULT_METHOD = "checkerboard"
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", type=str, dest="func",help="the function to compare", default=DEFAULT_FUNC)
+parser.add_argument("-i", type=str, dest="image",help="the image to compare", default=DEFAULT_COMPARISON_IMAGE)
+parser.add_argument("-m", type=str, dest="method", help="the comparison method", default=DEFAULT_METHOD) # allowed_values=["checkerboard","diff", "blend"]
 
-ca = bcp.cupy_array_from_ndarray(a)
-rca = bcp.rotate_90_deg(ca)
-cres = bcp.cupy_array_to_ndarray(rca)
+args = parser.parse_args()
+FUNC=args.func
+COMPARISON_IMAGE = args.image
+METHOD = args.method
 
-pa = bpl.pil_image_from_ndarray(a)
-rpa = bpl.rotate_90_deg(pa)
-pres = bpl.to_ndarray(rpa)
+a = utils.load_image_from_path(COMPARISON_IMAGE)
 
-sires = bsi.rotate_90_deg(a)
+def cupy():
+    import benchmark_cupy as bcp
+    ca = bcp.cupy_array_from_ndarray(a)
+    rca = getattr(bcp,FUNC)(ca)
+    cres = bcp.cupy_array_to_ndarray(rca)
+    return cres
 
-ma = bmp.gpuimage_from_ndarray(a)
-rma = bmp.rotate_90_deg(ma)
-mres = np.asarray(rma)
+def pillow():
+    import benchmark_pillow as bpl
+    pa = bpl.pil_image_from_ndarray(a)
+    rpa = getattr(bpl,FUNC)(pa)
+    pres = bpl.to_ndarray(rpa)
+    return pres
+
+def opencv():
+    import benchmark_opencv as bcv
+    return getattr(bcv,FUNC)(a)
+
+def scikit_image():
+    import benchmark_scikit_image as bsi
+    sires = getattr(bsi,FUNC)(a)
+    return sires
+
+def run_millipyde():
+    import benchmark_millipyde as bmp
+    ma = bmp.gpuimage_from_ndarray(a)
+    rma = getattr(bmp,FUNC)(ma)
+    mres = np.asarray(rma)
+    return mres
+
+def load_millipyde():
+    correct_path = utils.get_correct_image_path(COMPARISON_IMAGE, FUNC)
+    mpres = np.load(correct_path)
+    return mpres
 
 from matplotlib import pyplot as plt
 from matplotlib.gridspec import GridSpec
@@ -29,8 +60,9 @@ fig = plt.figure(figsize=(8, 9))
 
 from skimage import data, transform, exposure
 from skimage.util import compare_images
+
 def show_diff(a,aname,b,bname):
-    blend_rotated = compare_images(a, b, method='diff')
+    blend_rotated = compare_images(a, b, method=METHOD, n_tiles=(16,16))
 
     gs = GridSpec(3, 2)
     ax0 = fig.add_subplot(gs[0, 0])
@@ -42,15 +74,19 @@ def show_diff(a,aname,b,bname):
     ax1.imshow(b, cmap='gray')
     ax1.set_title(bname)
     ax2.imshow(blend_rotated, cmap='gray')
-    ax2.set_title('Blend comparison')
-    for a in (ax0, ax1, ax2):
-        a.axis('off')
+    ax2.set_title(METHOD + ' comparison')
+    for x in (ax0, ax1, ax2):
+        x.axis('off')
     plt.tight_layout()
     plt.plot()
+    a,b = map(utils.convert_image_type_to_float, (a,b))
+    percent_diff = utils.percent_mismatched(a,b)
+    avg_diff = np.mean(a-b)
+    stdev = np.std(a-b)
+    print(f"{aname} vs {bname} percent diff: {percent_diff} mean: {avg_diff} stdev: {stdev} {a.dtype} {b.dtype}")
+    plt.savefig(f"./outputs/{METHOD}-comparison-{aname}-{bname}-{FUNC}.png")
     plt.show()
-# show_diff(mres,"millipyde",cres,"cupy")
+show_diff(load_millipyde(),"millipyde",scikit_image(),"scikit-image")
 # show_diff(pres,"pillow",cres,"cupy")
-show_diff(sires,"scikit-image",cres,"cupy")
-zero = np.zeros_like(mres)
-pc_m = (mres > 0).sum() / zero.size
-pc_c = (sires > 0).sum() / zero.size
+# show_diff(scikit_image(),"scikit-image",opencv(),"opencv")
+
