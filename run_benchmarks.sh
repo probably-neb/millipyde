11c8e96bc1324fca11204d195401172baef42e3f
Ben Kunkle <ben.kunkle@gmail.com>
2023-03-31 11:32:04 -0500

    amd data collection

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -3,7 +3,8 @@
 export HIP_PLATFORM=$(eval "${ROCM_HOME}/hip/bin/hipconfig --platform")
 TIME_STAMP=$(date +"%Y%m%d_%H_%M")
 
-INPUT_SIZES="500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500 6000"
+INPUT_SIZES="500 1000 1500 2000 2500 3000 3500 4000 4500 5000 5500"
+#6000"
 # replacement for INPUT_SIZES used when testing
 INPUT_SIZE="500"
 
@@ -17,10 +18,10 @@ pip install -r requirements.txt --quiet
 # @param --benchmark-disable-gc: disable garbage collection during benchmarks
 py.test --benchmark-only \
 	-k 'cupy or millipyde or opencv_cuda' \
-	--rounds 1 \
-	--warmup-rounds 0 \
+	--rounds 100 \
+	--warmup-rounds 25 \
 	--benchmark-disable-gc \
-	--input-sizes ${INPUT_SIZE} \
+	--input-sizes ${INPUT_SIZES} \
 	--benchmark-save="${HIP_PLATFORM}_${TIME_STAMP}"
 
 cd ..
